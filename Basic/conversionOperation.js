f81717fc98ec57baaa68d8d9a73e36eaa7b099dc
tejastamnar18 <tejastamnar8034@gmail.com>
2025-08-10 14:23:16 +0000

    conversion of datatypes

---
+++ Basic/conversionOperation.js
@@ -0,0 +1,28 @@
+let score =true
+console.log( typeof score)
+console.log(typeof(score))
+
+let ValueInNumber  = Number(score)
+console.log(typeof ValueInNumber)
+console.log(ValueInNumber)
+
+//"33" = 33
+//"33bac"= NaN
+//true = 1 false = 0
+
+
+let isloggedIn = 1
+let isLoggedInBoolean = Boolean(isloggedIn)
+console.log(isLoggedInBoolean)
+console.log(typeof isLoggedInBoolean)
+
+
+//1 =true//0 = false
+//" " = false // "tejas" = true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
