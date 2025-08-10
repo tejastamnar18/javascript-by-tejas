5fde1b9bb51a41084b341a821d9b7907635253f9
tejastamnar18 <tejastamnar8034@gmail.com>
2025-08-10 11:03:41 +0000

    Conatiner Config Done

---
+++ Basic/test.js
@@ -0,0 +1,5 @@
+console.log("This is a test file in the Basic directory.");
+console.log("Make sure to run this file to see the output.");
+console.log("You can modify this file as needed for your tests.");
+console.log("Happy coding!");
+console.log("End of test file.");
