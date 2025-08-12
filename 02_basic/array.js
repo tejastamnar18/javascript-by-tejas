ddba160a94a79d92a4c9bd947114e8ad4205be60
tejastamnar18 <tejastamnar8034@gmail.com>
2025-08-12 07:02:01 +0000

    Add array.js with examples of array manipulation and methods in JavaScript

---
+++ 02_basic/array.js
@@ -0,0 +1,88 @@
+// array
+
+const myArr = [1, 2, 3, 4, 5];
+const myHeros =["shaktiman", "batman", "superman", "spiderman"];
+const myArr2 = new Array(1, 2, 3, 4, 5);
+
+//console.log(myArr[0]); // 1
+
+
+// array methods
+//console.log(myArr.push(6)); // 6
+//console.log(myArr.pop()); // 6
+//console.log(myArr.unshift(9)); 
+//console.log(myArr.shift()); // 9
+
+//console.log(myArr.includes(3)); // true
+//console.log(myArr.indexOf(3)); // 2
+//console.log(myArr.lastIndexOf(3)); // 2
+
+const newArr = myArr.join();
+
+
+//console.log(typeof newArr); 
+console.log ("a", myArr)
+
+const newArr2 = myArr.slice(1, 3); // [2, 3]
+console.log(newArr2); // [2, 3]
+console.log(myArr); 
+
+
+const newArr3 = myArr.splice(1, 3);// [2, 3, 4]
+console.log(newArr3);
+
+
+
+
+// Notes 
+// 1. Array is a collection of items
+// 2. Array is an object
+// 3. Array is a reference type
+// 4. Array is mutable
+// 5. Array is zero indexed
+// 6. Array can hold any data type
+// 7. Array can hold any number of items
+// 8. Array can be nested
+// 9. Array can be iterated using for loop, forEach, map, filter, reduce, etc.
+// 10. Array can be sorted using sort() method
+// 11. Array can be reversed using reverse() method
+// 12. Array can be concatenated using concat() method
+// 13. Array can be flattened using flat() method
+// 14. Array can be filled using fill() method
+// 15. Array can be copied using slice() method
+// 16. Array can be checked for existence of an item using includes() method
+// 17. Array can be transformed using map() method
+// 18. Array can be filtered using filter() method
+// 19. Array can be reduced to a single value using reduce() method
+// 20. Array can be sorted using sort() method
+// 21. Array can be reversed using reverse() method
+// 22. Array can be joined using join() method
+// 23. Array can be split using split() method
+// 24. Array can be searched using indexOf() and lastIndexOf() methods
+// 25. Array can be checked for existence of an item using includes() method
+// 26. Array can be checked for emptiness using length property
+// 27. Array can be checked for equality using JSON.stringify() method
+// 28. Array can be checked for identity using === operator
+// 29. Array can be checked for type using typeof operator
+// 30. Array can be checked for instance using instanceof operator
+// 31. Array can be checked for prototype using Object.getPrototypeOf() method
+// 32. Array can be checked for constructor using constructor property
+// 33. Array can be checked for length using length property
+// 34. Array can be checked for first item using [0] index
+// 35. Array can be checked for last item using [length - 1] index
+// 36. Array can be checked for first item using shift() method
+// 37. Array can be checked for last item using pop() method
+// 38. Array can be checked for first item using unshift() method
+// 39. Array can be checked for last item using push() method
+// 40. Array can be checked for first item using slice(0, 1) method
+// 41. Array can be checked for last item using slice(-1) method
+// 42. Array can be checked for first item using splice(0, 1) method
+// 43. Array can be checked for last item using splice(-1) method
+// 44. Array can be checked for first item using shift() method
+// 45. Array can be checked for last item using pop() method
+// 46. Array can be checked for first item using unshift() method
+// 47. Array can be checked for last item using push() method
+// 48. Array can be checked for first item using slice(0, 1) method
+// 49. Array can be checked for last item using slice(-1) method
+// 50. Array can be checked for first item using splice(0, 1) method
+// 51. Array can be checked for last item using splice(-1) method
