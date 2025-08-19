fe3c0c14c6b077d2a9ccb4a727c52612eb53fa48
tejastamnar18 <tejastamnar8034@gmail.com>
2025-08-19 03:36:45 +0000

    Add scope.js to demonstrate variable scoping with var, let, and const

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,10 @@
+var a = 100;
+if(true)
+{
+var a = 300;
+let  b = 200;
+const c = 100;
+}
+console.log(a); // 300
+//console.log(b); // ReferenceError: b is not defined
+//console.log(c); // ReferenceError: c is not defined
