8e0ed1912cf57be2e5912a2858660e5e07515a70
tejastamnar18 <tejastamnar8034@gmail.com>
2025-08-19 03:54:24 +0000

    Refactor scope.js to comment out console.log statements for clarity

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -4,8 +4,51 @@ if(true)
 var a = 300;
 let  b = 200;
 const c = 100;
-console.log("Inside if block", a);
+//console.log("Inside if block", a);
 }
-console.log(a); // 300
+//console.log(a); // 300
 //console.log(b); // ReferenceError: b is not defined
 //console.log(c); // ReferenceError: c is not defined
+
+
+
+// Nested Scope Example
+function one(){
+    const name = "Tejas"
+
+    function two(){
+        const age = 20;
+        console.log(name);
+    }
+    //console.log(age); // ReferenceError: age is not defined
+    two();
+
+}
+// one(); // Tejas
+
+if(true){
+    const username = "Tejas";
+    if(username === "Tejas"){
+        const age = 20;
+        //console.log(username + age); 
+        }
+    //console.log(age); // ReferenceError: age is not defined
+}
+//console.log(username); // ReferenceError: username is not defined
+
+
+
+// Interesting Example
+ 
+console.log(addOne(5)); // 6
+
+
+function addOne(num){
+    return num + 1;
+}
+
+
+addTwo(5);// ReferenceError: Cannot access 'addTwo' before initialization
+const addTwo = function(num){
+    return num + 2;
+}
