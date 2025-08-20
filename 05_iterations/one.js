22d3ccd39f104c81099196696aec3a39a6434eb3
tejastamnar18 <tejastamnar8034@gmail.com>
2025-08-20 04:57:18 +0000

    Add examples of for loop with break and continue in one.js

---
+++ 05_iterations/one.js
@@ -0,0 +1,20 @@
+// for Loop
+
+// for Loop with break
+// for (let i = 1; i<21; i++) {
+//     if (i === 5) {
+//         console.log("We have reached 5!");
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// for Loop with continue
+for (let i = 1; i<21; i++) {
+    if (i === 5) {
+        console.log("We have reached 5!");
+        continue;
+    }
+    console.log(i);
+}
+
