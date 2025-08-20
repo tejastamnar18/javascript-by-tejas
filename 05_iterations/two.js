91d9f04b7a5e86b86ad246b7a36c84d1c4a8b4e3
tejastamnar18 <tejastamnar8034@gmail.com>
2025-08-20 05:36:48 +0000

    Add examples of while and do-while loops in two.js

---
+++ 05_iterations/two.js
@@ -0,0 +1,15 @@
+// while loop
+
+while (true) {
+  console.log('This will run forever');
+  // Uncomment the next line to break the loop
+  // break;
+}
+
+// do while loop
+let score = 0;
+do {
+    console.log(`Current score: ${score}`);
+    score++;
+}
+while (score < 10);
